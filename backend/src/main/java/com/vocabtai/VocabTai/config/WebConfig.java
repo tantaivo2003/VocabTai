ca46f91838524f5f3750cee27b43d11b242ac386
tantaivo2003 <tantaivo2003@gmail.com>
2025-07-11 12:02:35 +0700

    Update Docker File

--- backend/src/main/java/com/vocabtai/VocabTai/config/WebConfig.java
+++ backend/src/main/java/com/vocabtai/VocabTai/config/WebConfig.java
@@ -10,6 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Cho phép tất cả endpoint
                 .allowedOrigins("http://localhost:5173") // Cho phép từ FE
+                .allowedOrigins("http://localhost:3000") // Cho phép từ FE
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true); // Nếu có dùng cookie hoặc auth header
